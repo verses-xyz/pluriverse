65bd88f80e0e8bb5ebbf62dc938833fd58ea5511
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2022-01-01 21:15:45 -0800

    css tweaks

--- browser/src/components/Hero.jsx
+++ browser/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "./Hero.css";
 export default function Hero() {
   return (
     <div className="hero fadeInDown flex flex-col items-center justify-center">
-      <h1 className="text-7xl pb-5 m-0">
+      <h1 className="text-8xl pb-5 m-0">
         Pluriverse <span className="text-3xl">[noun]</span>
       </h1>
       <div className="text-3xl pb-8">plu·​ri·​verse | \ ˈplu̇rəˌvərs \</div>
