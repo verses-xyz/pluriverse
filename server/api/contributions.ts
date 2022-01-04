390082f783262e35e196848f66c8197b0e135c13
Spencer Chang <spencerc99@gmail.com>
2022-01-04 02:00:51 -0800

    try using vercel serverless functions

---
+++ server/api/contributions.ts
@@ -0,0 +1,14 @@
+import { addContribution } from "api-handlers/add_contribution";
+import { getContribution } from "api-handlers/get_contribution";
+import { getContributions } from "api-handlers/get_contributions";
+import express from "express";
+const { app, services } = require("../index");
+
+const contributionsRouter = express.Router();
+contributionsRouter.get("/", getContributions(services));
+contributionsRouter.get("/:id", getContribution(services));
+contributionsRouter.post("/", addContribution(services));
+
+app.use("/contributions", contributionsRouter);
+
+module.exports = app;
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
-import { addContribution } from "./api/add_contribution";
-import { getContributions } from "./api/get_contributions";
-import { getContribution } from "./api/get_contribution";
+// import { addContribution } from "./api-handlers/add_contribution";
+// import { getContributions } from "./api-handlers/get_contributions";
+// import { getContribution } from "./api-handlers/get_contribution";
 
 const app = express();
 app.use(express.json());
@@ -17,17 +17,21 @@ const port = 3001;
 const prisma = new PrismaClient();
 const services = { prisma };
 
-const contributionsRouter = express.Router();
-contributionsRouter.get("/", getContributions(services));
-contributionsRouter.get("/:id", getContribution(services));
-contributionsRouter.post("/", addContribution(services));
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/contributions", cors(corsOptions), contributionsRouter);
+app.use(cors(corsOptions));
+
+// TODO: commmenting out to try vercel serverless,
+// const contributionsRouter = express.Router();
+// contributionsRouter.get("/", getContributions(services));
+// contributionsRouter.get("/:id", getContribution(services));
+// contributionsRouter.post("/", addContribution(services));
+// app.use("/contributions", contributionsRouter);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+module.exports = { app, services };
