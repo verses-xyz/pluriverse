928fcbc50fa040eac3e7c390aab59d5ebdb0bcd0
Jacky Zhao <j.zhao2k19@gmail.com>
2022-01-07 20:49:35 -0500

    Update server/api-handlers/twitter-verify.ts
    Co-authored-by: Spencer Chang <spencerc99@gmail.com>

--- server/api-handlers/twitter-verify.ts
+++ server/api-handlers/twitter-verify.ts
@@ -26,7 +26,7 @@ export function verify({prisma}: Services): RequestHandler {
 
         if (existingUser.twitterVerified) {
             console.log(`already verified user: @${existingUser.twitterUsername}`);
-            res.json({ status: "ok", msg: "already verified"});
+            res.status(200).json({msg: "already verified"})
             return;
         }
 
