bb55dc28576a143dc382af3f21741f37ab6194de
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2022-01-09 16:25:12 -0800

    add button

--- browser/src/components/Hero.jsx
+++ browser/src/components/Hero.jsx
@@ -2,13 +2,23 @@ import "./Hero.css";
 
 export default function Hero() {
   return (
-    <div className="hero fadeInDown flex flex-col items-center justify-center">
-      <h1 className="text-8xl pb-5 m-0">
-        Pluriverse <span className="text-3xl">[noun]</span>
-      </h1>
-      <div className="text-3xl pb-8">plu·​ri·​verse | \ ˈplu̇rəˌvərs \</div>
-      <div className="text-7xl pb-8">“a world where many worlds fit”</div>
-      <div className="pt-4 text-2xl">– Zapatistas, Chiapas</div>
+    <div>
+      <div class="flex justify-end pt-8 pr-8">
+        <div class="pr-4">
+          <button className="glass-button">About</button>
+        </div>
+        <div>
+          <button className="glass-button">Contribute</button>
+        </div>
+      </div>
+      <div className="hero fadeInDown flex flex-col items-center justify-center">
+        <h1 className="text-8xl pb-5 m-0">
+          Pluriverse <span className="text-3xl">[noun]</span>
+        </h1>
+        <div className="text-3xl pb-8">plu·​ri·​verse | \ ˈplu̇rəˌvərs \</div>
+        <div className="text-7xl pb-8">“a world where many worlds fit”</div>
+        <div className="pt-4 text-2xl">– Zapatistas, Chiapas</div>
+      </div>
     </div>
   );
 }
