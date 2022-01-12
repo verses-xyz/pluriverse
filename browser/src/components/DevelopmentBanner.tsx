ba7231559f41f40c43c958fea58df09db973625d
Spencer Chang <spencerc99@gmail.com>
2022-01-11 23:36:27 -0800

    add new development banner

--- browser/src/components/DevelopmentBanner.tsx
+++ browser/src/components/DevelopmentBanner.tsx
@@ -6,22 +6,30 @@ export function DevelopmentBanner() {
       <div className="banner">
         <p>
           This artifact is currently a work in progress and should not be shared
-          with you. If this link has been shared with you, we highly value your
-          feedback and participation with this artifact. For more context on the
-          artifact, to leave feedback, and/or to schedule a call with one of the
-          core stewards to see how you might contribute, please see our{" "}
+          publicly. If this link has been shared with you, we would highly
+          treasure your feedback and participation with this artifact.
+        </p>
+        <p>
+          For more context on the artifact, to leave feedback, and/or to
+          schedule a call with one of the core stewards to see how you might
+          contribute, please see our{" "}
           <a href="https://docs.google.com/document/d/1Tes7GTDx_cQNlTVwE6_jMFuLN1N3SQUzrA3dypYv3Nw/edit">
             running working document
           </a>
-          . We are currently collecting signatures on the artifact (please go
+          .
+        </p>
+        <p>
+          We are currently collecting signatures on the artifact (please go
           through the signing flow and verify on Twitter at the bottom of this
           page!), and{" "}
           <a href="https://docs.google.com/document/d/1cWbH_yCwsRHyt9pFgKwHujOylje9rZq-KbZwa1CAwEo/edit">
             workshopping examples of contributions
           </a>{" "}
-          with organizations.We are currently collecting signatures on the
-          artifact (please go through the signing flow and verify on Twitter at
-          the bottom of this page!), and with organizations.
+          with organizations.
+        </p>
+        <p>
+          We would love your feedback and to invite you to participate. If there
+          are factors blocking participation, please let us know!
         </p>
         <p>
           We are planning on freezing the text by <b>Jan 13</b>, and to launch
