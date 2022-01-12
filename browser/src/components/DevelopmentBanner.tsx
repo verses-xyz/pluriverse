c97758076af343db008d09bf780348c400037485
Spencer Chang <spencerc99@gmail.com>
2022-01-11 16:23:17 -0800

    add new development banner copy

--- browser/src/components/DevelopmentBanner.tsx
+++ browser/src/components/DevelopmentBanner.tsx
@@ -4,19 +4,22 @@ export function DevelopmentBanner() {
   return (
     <>
       <div className="banner">
+        "If this has been shared with you, running working document.
         <p>
-          If this has been shared with you, this link has been shared with you
-          since we value your feedback and participation with this artifact. For
-          more context on the work, please see our{" "}
+          If this has been shared with you, twe highly value your feedback and
+          participation with this artifact. For more context on the work, to
+          leave feedback, and/or to schedule a call with one of the co-creators
+          of this artifact to see how you might contribute, please see our{" "}
           <a href="https://docs.google.com/document/d/1Tes7GTDx_cQNlTVwE6_jMFuLN1N3SQUzrA3dypYv3Nw/edit">
             running working document
           </a>
-          . We are currently collecting signatures on the artifact, and{" "}
+          . We are currently collecting signatures on the artifact (please go
+          through the signing flow and verify on Twitter at the bottom of this
+          page!), and{" "}
           <a href="https://docs.google.com/document/d/1cWbH_yCwsRHyt9pFgKwHujOylje9rZq-KbZwa1CAwEo/edit">
-            workshopping contributions
+            workshopping examples of contributions
           </a>
-          with organizations to make sure that the pattern language is seeded in
-          a high quality manner.
+          with organizations.
           <br />— the <a href="https://verses.xyz">verses</a> pluriverse team
         </p>
       </div>
