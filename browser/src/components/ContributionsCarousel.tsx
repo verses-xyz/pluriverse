dfede5e12af5fe3c6ee378bf6b979de778aec5ab
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2022-01-12 22:54:19 -0800

    add grdaient

--- browser/src/components/ContributionsCarousel.tsx
+++ browser/src/components/ContributionsCarousel.tsx
@@ -7,16 +7,32 @@ export default function ContributionsCarousel({
   contributions: Contribution[];
 }) {
   return (
-    <div className="flex flex-row" style={{ overflowX: "auto", width: "100%" }}>
-      {contributions.map((contribution) => (
-        <div className="pr-4">
-          <CompactContributionCard
-            key={contribution.id}
-            contribution={contribution}
-            hideHeader
-          />
-        </div>
-      ))}
+    <div style={{ position: "relative" }}>
+      <div
+        className="flex flex-row"
+        style={{ overflowX: "auto", width: "100%" }}
+      >
+        {contributions.map((contribution) => (
+          <div className="pr-4">
+            <CompactContributionCard
+              key={contribution.id}
+              contribution={contribution}
+              hideHeader
+            />
+          </div>
+        ))}
+      </div>
+      <div
+        style={{
+          top: 0,
+          right: 0,
+          position: "absolute",
+          height: "100%",
+          width: "100px",
+          zIndex: 100000,
+          backgroundImage: `linear-gradient(to right, rgba(255,0,0,0), rgb(32 32 44)  100%`,
+        }}
+      />
     </div>
   );
 }
