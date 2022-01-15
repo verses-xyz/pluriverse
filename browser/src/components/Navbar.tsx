08d28133c7000f646fd2ec66a730473d306fdcad
Spencer Chang <spencerc99@gmail.com>
2022-01-14 17:51:03 -0800

    move navbar items for mobile for now, back to react_app

--- browser/src/components/Navbar.tsx
+++ browser/src/components/Navbar.tsx
@@ -1,19 +1,8 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
     <nav className="flex justify-end pt-8 pr-8">
-      <div className="pr-4">
-        <NavLink to="/about">
-          {({ isActive }) => (
-            <button
-              className={`glass-button ${isActive ? "selectedBorder" : ""}`}
-            >
-              About
-            </button>
-          )}
-        </NavLink>
-      </div>
       <div className="pr-4">
         <NavLink to="/">
           {({ isActive }) => (
@@ -26,18 +15,29 @@ export function Navbar() {
         </NavLink>
       </div>
       <div className="pr-4">
-        {/* Can make this scroll instead. */}
-        <a href="/#contribute">
-          <button className={`glass-button`}>Contribute</button>
-        </a>
-      </div>
-      <div className="pr-4">
         <NavLink to="/contributions">
           {({ isActive }) => (
             <button
               className={`glass-button ${isActive ? "selectedBorder" : ""}`}
             >
               All Contributions
+            </button>
+          )}
+        </NavLink>
+      </div>
+      <div className="pr-4">
+        {/* Can make this scroll instead. */}
+        <a href="/#contribute">
+          <button className={`glass-button`}>Contribute</button>
+        </a>
+      </div>
+      <div className="pr-4">
+        <NavLink to="/about">
+          {({ isActive }) => (
+            <button
+              className={`glass-button ${isActive ? "selectedBorder" : ""}`}
+            >
+              About
             </button>
           )}
         </NavLink>
