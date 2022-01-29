1bd8bfc8d0964737152efdf3dc43874132a3ecbb
Spencer Chang <spencerc99@gmail.com>
2022-01-29 13:52:23 -0800

    fix contribtuion link on prod lol

--- browser/src/helpers/contributions.ts
+++ browser/src/helpers/contributions.ts
@@ -1,7 +1,7 @@
 import { Contribution } from "src/types/common/server-api";
 
 export function getContributionLink(contribution: Contribution) {
-  return process.env.NODE_ENV === "production"
-    ? "https://"
-    : "http://" + window.location.host + `/contributions/${contribution.id}`;
+  return (
+    "https://" + window.location.host + `/contributions/${contribution.id}`
+  );
 }
