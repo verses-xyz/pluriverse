9ad121c86a1dc4a6e027075d64dfb5b54c187358
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2022-01-31 18:18:33 -0800

    update navbar

--- browser/src/components/Navbar.tsx
+++ browser/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <nav className="flex justify-end pt-8 pr-8">
-      <div className="pr-4">
+    <nav className="flex justify-between justify-end pt-8 px-8">
+      <div>
         <NavLink to="/">
           {({ isActive }) => (
             <button
@@ -14,33 +14,35 @@ export function Navbar() {
           )}
         </NavLink>
       </div>
-      <div className="pr-4">
-        <NavLink to="/contributions">
-          {({ isActive }) => (
-            <button
-              className={`glass-button ${isActive ? "selectedBorder" : ""}`}
-            >
-              All Contributions
-            </button>
-          )}
-        </NavLink>
-      </div>
-      <div className="pr-4">
-        {/* Can make this scroll instead. */}
-        <a href="/#contribute">
-          <button className={`glass-button`}>Contribute</button>
-        </a>
-      </div>
-      <div className="pr-4">
-        <NavLink to="/about">
-          {({ isActive }) => (
-            <button
-              className={`glass-button ${isActive ? "selectedBorder" : ""}`}
-            >
-              About
-            </button>
-          )}
-        </NavLink>
+      <div className="flex ">
+        <div className="pr-4">
+          <NavLink to="/about">
+            {({ isActive }) => (
+              <button
+                className={`glass-button ${isActive ? "selectedBorder" : ""}`}
+              >
+                About
+              </button>
+            )}
+          </NavLink>
+        </div>
+        <div className="pr-4">
+          <NavLink to="/contributions">
+            {({ isActive }) => (
+              <button
+                className={`glass-button ${isActive ? "selectedBorder" : ""}`}
+              >
+                All Contributions
+              </button>
+            )}
+          </NavLink>
+        </div>
+        <div>
+          {/* Can make this scroll instead. */}
+          <a href="/#contribute">
+            <button className={`glass-button`}>Contribute</button>
+          </a>
+        </div>
       </div>
     </nav>
   );
