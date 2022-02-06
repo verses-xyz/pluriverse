2525e932a136b78d32a14fa72d794960656c7fbd
Alejandro Garcia Salas <garciasalasalejandro@gmail.com>
2022-02-06 10:57:05 -0800

    center hero title on mobile

--- browser/src/components/Hero.tsx
+++ browser/src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div>
       <div className="hero fadeInDown flex flex-col items-center justify-center">
-        <h1 className="text-5xl md:text-8xl m-0">
+        <h1 className="text-5xl text-center md:text-8xl m-0">
           Towards a Digital Pluriverse
         </h1>
         <div className="text-2xl md:text-5xl py-8">
