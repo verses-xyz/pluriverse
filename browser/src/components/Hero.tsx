57758b6860d8e2741a6110abb26457018af4f181
Spencer Chang <spencerc99@gmail.com>
2022-02-10 23:35:37 -0500

    add verses logo

--- browser/src/components/Hero.tsx
+++ browser/src/components/Hero.tsx
@@ -4,6 +4,14 @@ export default function Hero() {
   return (
     <div>
       <div className="hero fadeInDown flex flex-col items-center justify-center">
+        <div className="versesLogoContainer">
+          <a href="https://verses.xyz">
+            <img
+              className="w-24 h-24 md:w-32 md:h-32 mb-2"
+              src="/verses-logo.svg"
+            />
+          </a>
+        </div>
         <h1 className="text-5xl text-center md:text-8xl m-0">
           Towards a Digital Pluriverse
         </h1>
